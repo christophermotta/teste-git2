36382ee30cc8a9ad500c262b64eae3247a2deaff
Christopher <mottachristopher@edu.unisinos.br>
2024-07-18 20:51:03 -0300

    Contrutor classe Employee

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,5 +1,13 @@
 package entities;
 
 public class Employee {
-
+	private String name;
+	private Double salary;
+	
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	
 }
