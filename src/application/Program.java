ba724bacd6f95677d0209b0504ef10fe4fdcf839
Christopher <mottachristopher@edu.unisinos.br>
2024-07-18 20:43:38 -0300

    Primeiro Commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
 	}
 
 }
